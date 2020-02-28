f11279e08914a1682e18050143853a9b63eb4dcd
eevan7a9 <e7a9@protonmail.com>
2020-02-29 02:26:58 +0900

    added label

--- filter.js
+++ filter.js
@@ -1,7 +1,7 @@
 const animals = ["frog", "snake", "pig", "rat", "bear", "wolf"];
-
+// returning new Array
 const getFrog = animals.filter(animal => animal == "frog");
 // console.log(getFrog); ["frog"]
-
+// returning new Array
 const getByStrLength = animals.filter(animal => animal.length == 4);
-console.log(getByStrLength);
+// console.log(getByStrLength); ["frog", "bear", "wolf"];
